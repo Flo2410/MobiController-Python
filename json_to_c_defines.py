8fb358d14138ba154461fd2d7221d7c6b2571c0e
Flo2410 <florian@hye.dev>
2023-04-05 12:12:08 +0200

    added prefix

--- json_to_c_defines.py
+++ json_to_c_defines.py
@@ -6,9 +6,10 @@ def main():
 
     for type in data:
         print(f"// {type}")
-        for cmd in data.get(type):
-            name = cmd.get("name").replace(" ", "_").upper()
-            print(f"#define {name} {hex(cmd.get('min_id'))}")
+        for frame in data.get(type):
+            name = f"{type.replace('s', '').upper()}_"
+            name += frame.get("name").replace(" ", "_").upper()
+            print(f"#define {name} {hex(frame.get('min_id'))}")
         print("")
 
 if __name__ == "__main__":
