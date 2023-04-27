5176b9681b4a40e6f03d7196b93e3a9721e03a49
Flo2410 <florian@hye.dev>
2023-04-27 11:06:26 +0200

    json_to_cpp: generate codes attr

--- json_to_cpp_enum.py
+++ json_to_cpp_enum.py
@@ -1,5 +1,14 @@
 import json
 
+def extract_codes(name, codes):
+    print(f"enum class {name} {'{'}")
+
+    for index, code in enumerate(codes):
+        code_name = code.replace(" ", "_").replace("-", "_").upper()
+        print(f"{code_name} = {hex(index)},")
+
+    print("};\n")
+
 def main():
     f = open("protocol.json")
     data = json.load(f)
@@ -8,25 +17,41 @@ def main():
         print(f"// {type}")
         print(f"enum class {type.upper()} {'{'}")
         for frame in data.get(type):
-            name = frame.get("name").replace(" ", "_").upper()
-            print(f"{name} = {hex(frame.get('min_id'))},")
+            frame_name = frame.get("name").replace(" ", "_").upper()
+            print(f"{frame_name} = {hex(frame.get('min_id'))},")
         print("};\n")
 
     for frame in data.get([type for type in data][0]):
-        name = frame.get("name").replace(" ", "_").upper()
+        frame_name = frame.get("name").replace(" ", "_").upper()
 
         for payload in frame.get("payload"):
             bits = payload.get("bits")
-            if bits == None:
-                continue
+            codes = payload.get("codes")
 
-            print(f"enum class {name}_SUB_DEVICES {'{'}")
-            
-            for index, bit in enumerate(bits):
-                bit_name = bit.replace(" ", "_").replace("-", "_").upper()
-                print(f"{bit_name} = {hex(0x01 << index)},")
+            if bits != None:
+                print(f"enum class {frame_name}_SUB_DEVICES {'{'}")
+                
+                for index, bit in enumerate(bits):
+                    bit_name = bit.replace(" ", "_").replace("-", "_").upper()
+                    print(f"{bit_name} = {hex(0x01 << index)},")
 
-            print("};\n")
+                print("};\n")
+
+            if codes != None:
+                payload_name = payload.get("name").replace(" ", "_").upper()
+                extract_codes(payload_name, codes)
+    
+    for frame in data.get([type for type in data][1]):
+        frame_name = frame.get("name").replace(" ", "_").upper()
+
+        for payload in frame.get("payload"):
+            codes = payload.get("codes")
+
+            if codes != None:
+                payload_name = payload.get("name").replace(" ", "_").upper()
+                extract_codes(payload_name, codes)
+
+
 
 if __name__ == "__main__":
     main()
